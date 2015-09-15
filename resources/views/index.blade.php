7c408998a274175144a8817a1bf8367cf4c5c708
Colin Schoen <cschoen@berkeley.edu>
2015-09-15 03:05:07 -0700

    use link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -152,10 +152,10 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="row">
-                <div class="col-md-4 center-text diagModalBtn wow fadeInUp" data-modal="diagModal" id="diagModalBtn">
+                <a id="diagModalBtn" href="#"><div class="col-md-4 center-text diagModalBtn wow fadeInUp" data-modal="diagModal">
                     <div class="large-icon"><i class="fa fa-road fa-fw"></i></div>
                     <p><strong>Diagnosis and Strategy</strong></p>
-                </div>
+                </div></a>
                 <div class="col-md-4 center-text coachModalBtn wow fadeInUp">
                     <div class="large-icon"><i class="fa fa-line-chart fa-fw"></i></div>
                     <p><strong>Coaching</strong></p>
